0a513b017c1d2ddb1b9055aa2c1fc5b169441af1
Joshua Milas <Josh.Milas@gmail.com>
2015-08-23 22:26:57 -0400

    Included events and todos on the home page

--- website/index.py
+++ website/index.py
@@ -5,6 +5,7 @@ Index for the website
 # Project imports
 import flask
 import logging
+import datetime
 
 # Local imports
 from __init__ import app
@@ -13,4 +14,14 @@ import models
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', title='Home')
+    tasks = models.Todo.select().where(models.Todo.event == None)  # TODO don't get done tasks if more than a week old
+    nearing_events = models.Event.select().order_by(models.Event.date.asc()).where(models.Event.date.between(datetime.date.today(),
+                                                datetime.date.today() + datetime.timedelta(7)))
+    return flask.render_template('index.html', title='Home', tasks=tasks, events=nearing_events)
+
+
+@app.route('/todo/by-id/<int:todo_id>')
+def todo_by_id(todo_id):
+    task = models.Todo.get(models.Todo.id == todo_id)
+    return task.task
+
