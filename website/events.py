b9691df122aa51e5f19e5ee983376c72f54f3485
Joshua Milas <Josh.Milas@gmail.com>
2015-09-03 01:11:39 -0400

    Added the frame of an AJAX call to update the value on checkbox change. Also added a todo html page to view all todos.

--- website/events.py
+++ website/events.py
@@ -16,7 +16,33 @@ def events():
     return flask.render_template('events.html', title='Events')
 
 
+@app.route('/events/by-id/<int:event_id>')
+def event_by_id(event_id):
+    event = models.Event.get(models.Event.id == event_id)
+    return event.name
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    if flask.request.method == 'POST':
+        print('POST:', flask.request.form)
+    todos = models.Todo.select()
+    return flask.render_template('todo.html', title='Todo', todos=todos)
+
+
 @app.route('/todo/by-id/<int:todo_id>')
 def todo_by_id(todo_id):
     task = models.Todo.get(models.Todo.id == todo_id)
     return task.task
+
+
+@app.route('/todo/status', methods=['POST'])
+def todo_status():
+    """Ajax request"""
+    try:
+        id = int(flask.request.form['id'])
+        status = True if flask.request.form['status'] == 'true' else False
+    except KeyError:
+        return flask.jsonify()
+    print(id, status)
+    return flask.jsonify()
