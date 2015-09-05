374f92ba98bb69d4a790ec4865f5fdd12035cf7a
Joshua Milas <Josh.Milas@gmail.com>
2015-09-05 19:19:22 -0400

    Added the strikeout in the CSS file, moved some html code in the base, added the functionality to strikeout a row on the index page to a task that is already done, and moved the code to a js file. Also added the check to grab to get tasks if they are not done or they were done within a week.

--- website/index.py
+++ website/index.py
@@ -7,6 +7,8 @@ import flask
 import logging
 import datetime
 
+logger = logging.getLogger(__name__)
+
 # Local imports
 from __init__ import app
 import models
@@ -14,10 +16,11 @@ import models
 
 @app.route('/')
 def index():
-    tasks = models.Todo.select().where(models.Todo.event == None)  # TODO don't get done tasks if more than a week old
+    tasks = models.Todo.select().where((models.Todo.event == None) & ((models.Todo.done == False) |
+                                        (models.Todo.date_done > (datetime.date.today() - datetime.timedelta(days=7)))))
     nearing_events = models.Event.select().order_by(models.Event.date.asc()).where(models.Event.date.between(datetime.date.today(),
                                                 datetime.date.today() + datetime.timedelta(7)))
-    return flask.render_template('index.html', title='Home', tasks=tasks, events=nearing_events)
+    return flask.render_template('index.html', title='Home', todos=tasks, events=nearing_events)
 
 
 @app.before_request
