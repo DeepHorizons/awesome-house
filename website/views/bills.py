1c0232b9d7c751bad7dfd73795135e43b1b725e7
Joshua Milas <Josh.Milas@gmail.com>
2015-11-21 21:06:38 -0500

    All pages now require an authorized user, except for the index page, registration, checking login, and logging out

--- website/views/bills.py
+++ website/views/bills.py
@@ -5,6 +5,7 @@ Index for the website
 # Project imports
 import flask
 import logging
+import flask_login
 
 # Local imports
 from __init__ import app
@@ -14,5 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 @app.route('/bills')
+@flask_login.login_required
 def bills():
     return flask.render_template('bills.html', title='Bills')
--- website/views/electricity.py
+++ website/views/electricity.py
@@ -5,6 +5,7 @@ Index for the website
 # Project imports
 import flask
 import logging
+import flask_login
 
 # Local imports
 from __init__ import app
@@ -14,5 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 @app.route('/electricity')
+@flask_login.login_required
 def electricity():
     return flask.render_template('electricity.html', title='Electricity')
