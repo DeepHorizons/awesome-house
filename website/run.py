c36989a6f7000255aca2cb7ca1bd46b2d4281a62
Joshua Milas <Josh.Milas@gmail.com>
2015-11-26 11:26:08 -0500

    Added logging to run.py

--- website/run.py
+++ website/run.py
@@ -5,10 +5,14 @@ A file to start the server
 from __init__ import app
 import logging
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     if app.config['DEBUG']:
-        print(app.config)
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+        logger.debug('Running in debug mode')
+        logger.debug(app.config)
     else:
         logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+        logger.info('Running in production mode')
     app.run(host='0.0.0.0', threaded=True)
