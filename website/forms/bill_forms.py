5e12dc084ff68a0c6257a8d69b6d866de55497c5
Joshua Milas <Josh.Milas@gmail.com>
2015-12-26 12:02:13 -0500

    Added the bill modal

--- website/forms/bill_forms.py
+++ website/forms/bill_forms.py
@@ -4,3 +4,12 @@ import wtforms
 
 class PaymentForm(flask_wtf.Form):
     token = wtforms.StringField(description="Your venmo userID or leave blank to signify paying cash")
+
+
+class BillForm(flask_wtf.Form):
+    due = wtforms.DateField(None, [wtforms.validators.DataRequired()], format='%Y-%m-%d',
+                            description='The date the bill is due')
+    name = wtforms.StringField(None, [wtforms.validators.DataRequired()])
+    amount = wtforms.FloatField(None, [wtforms.validators.DataRequired])
+    description = wtforms.StringField(None, [wtforms.validators.Length(max=4096), wtforms.validators.Optional()],
+                                      description='A description of the bill', widget=wtforms.widgets.TextArea())
