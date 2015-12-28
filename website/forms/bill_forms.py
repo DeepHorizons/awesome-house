49d182aeab69697ea28d02a5880d99d5c1cef376
Joshua Milas <Josh.Milas@gmail.com>
2015-12-28 00:02:03 -0500

    $5000 limit on bills

--- website/forms/bill_forms.py
+++ website/forms/bill_forms.py
@@ -10,6 +10,6 @@ class BillForm(flask_wtf.Form):
     due = wtforms.DateField(None, [wtforms.validators.DataRequired()], format='%Y-%m-%d',
                             description='The date the bill is due')
     name = wtforms.StringField(None, [wtforms.validators.DataRequired()])
-    amount = wtforms.FloatField(None, [wtforms.validators.DataRequired])
+    amount = wtforms.FloatField(None, [wtforms.validators.DataRequired(), wtforms.validators.NumberRange(0, 5000)])
     description = wtforms.StringField(None, [wtforms.validators.Length(max=4096), wtforms.validators.Optional()],
                                       description='A description of the bill', widget=wtforms.widgets.TextArea())
