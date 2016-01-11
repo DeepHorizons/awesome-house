0a30ef36086cb26ca791fe361745fe8e529bf880
Joshua Milas <Josh.Milas@gmail.com>
2016-01-10 20:30:14 -0500

    Use the payment methods token if it exists and use the maintainers otherwise (this is a fix for the venmo sandbox)

--- website/views/bills.py
+++ website/views/bills.py
@@ -72,13 +72,14 @@ def bills():
 
     # TODO Improve all of this
     payment_methods = list(models.PaymentMethod.select(models.PaymentMethod, models.User).join(models.User).join(models.Permission).where(models.Permission.permission == models.PERMISSION_TYPE['bills']))  # TODO figure out how to handle multiple payment methods
-    outstanding_charges = models.Charges.select().where(models.Charges.paid == False).execute()
+    outstanding_charges = models.Charges.select(models.Charges, models.PaymentMethod).join(models.PaymentMethod).where(models.Charges.paid == False).execute()
 
     # Get an update on all charges that were made online
     for charge in outstanding_charges:
         if charge.online_charge_id:
             try:
-                response_json = bill_functions.venmo_get_payment_info(charge.token, charge.online_charge_id)
+                token = charge.payment_method.token if charge.payment_method.token else charge.bill.maintainer.payment_methods[0].token
+                response_json = bill_functions.venmo_get_payment_info(token, charge.online_charge_id)
             except LookupError as e:
                 logger.critical('Could not get charge information: {}'.format(e))
                 flask.flash('Could not get charge information')
