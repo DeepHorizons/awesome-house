43fa948b3fbe423575fea6cb8217fddf78139a75
Joshua Milas <Josh.Milas@gmail.com>
2016-01-15 00:55:55 -0500

    Add the REST endpoints

--- website/views/music.py
+++ website/views/music.py
@@ -11,6 +11,7 @@ import requests
 # Local imports
 from __init__ import app
 import models
+import misc.music_functions as music_functions
 
 logger = logging.getLogger(__name__)
 
@@ -26,3 +27,19 @@ def music():
 def get_music():
     r = requests.get('http://127.0.0.1:8000/', stream=True)
     return flask.Response(flask.stream_with_context(r.iter_content()), content_type=r.headers['content-type'])
+
+
+@app.route('/music/control/next', methods=['PUT'])
+@flask_login.login_required
+def next_endpoint():
+    music_functions.mpd_next()
+    return flask.jsonify(status='0')
+
+
+@app.route('/music/info/current_song', methods=['GET'])
+@flask_login.login_required
+def info_current_song():
+    artist = music_functions.mpd_current_artist()
+    song = music_functions.mpd_current_song()
+    percent_done = music_functions.mpd_get_percent_done()
+    return flask.jsonify(song=song, artist=artist, percent_done=percent_done)
